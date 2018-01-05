b8785e783b33c106c3ca7e42a38718d3b645699e
Pedro Leite <pedro.larva@gmail.com>
2018-01-05 16:44:38 -0200

    first deploy

--- thrust.sh
+++ thrust.sh
@@ -1,2 +1,3 @@
-nohup java -Dport=5000 -jar ./jarlib/thrust.jar ./app/server.js & 
-echo OK
+/sbin/ifconfig
+/usr/bin/java -Dport=5000 -Djava.security.egd=file:/dev/urandom -jar ./jarlib/thrust.jar ./app/server.js &
+exit 0
